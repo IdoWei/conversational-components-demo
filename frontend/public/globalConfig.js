ff28b8fa9a74b1608cfcea3ceecea102af08787f
Ido Weitzman <ido5091@gmail.com>
2020-07-11 17:41:18 +0300

    added global config

---
+++ frontend/public/globalConfig.js
@@ -0,0 +1,2 @@
+//this file enables a developer to change API_URL even after the app has been bundled, because this file is loaded as script in index.html
+window.API_URL = "http://localhost:4000";
--- frontend/src/services/BackendService.js
+++ frontend/src/services/BackendService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:4000";
-  
+const { API_URL } = window;
+
 const BackendService = {
     EchoMessage: (message) => axios.post(`${API_URL}/messages/echo-message`, {message})
         .then(function (response) {
